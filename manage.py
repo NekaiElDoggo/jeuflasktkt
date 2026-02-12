01f9e8e1fd1ba04ff976dbf19a30f6d3a75c0e8b
Adrien Mathieu <nekaisqd@gmail.com>
2026-02-12 12:15:42 +0100

    1

--- manage.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == '__main__':
-    print('Use flask db commands via environment:')
-    print('set FLASK_APP=manage.py; flask db init')
-
